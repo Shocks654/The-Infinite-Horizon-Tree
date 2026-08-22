f4c4957d69994146651d2aa5807422969040d72b
Shocks654 <sukinyuszi16@gmail.com>
2026-08-22 19:32:57 +0200

    Day 2: Part 5 Adding the Prestige layer again

--- js/tree.js
+++ js/tree.js
@@ -1,10 +1,15 @@
 // treeLayout will override the default tree's layout if used
 var layoutInfo = {
-    startTab: "none", // Set to "none" because the layer position is handled inside the prestige file
+    startTab: "none", // Set to "none" so the game starts on the main map instead of inside a layer
     startNavTab: "tree-tab", // Keeps the main navigation focused on the tree layout
 	showTree: true,
     treeLayout: ""
 }
+
+// Defines the layout of the tree with rows and layers
+var testTree = [
+    ["p"] // Row 0 contains only the Prestige (p) layer. This fixes the missing button bug!
+]
 
 // A "ghost" layer which offsets other layers in the tree
 addNode("blank", {
