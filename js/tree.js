a0038f6aa44a830aaab141af41906dc0a14145a2
Shocks654 <sukinyuszi16@gmail.com>
2026-08-22 19:31:06 +0200

    Day 2: Part 4 Making the start

--- js/tree.js
+++ js/tree.js
@@ -1,23 +1,19 @@
+// treeLayout will override the default tree's layout if used
 var layoutInfo = {
-    startTab: "p",
-    startNavTab: "tree-tab",
+    startTab: "none", // Set to "none" because the layer position is handled inside the prestige file
+    startNavTab: "tree-tab", // Keeps the main navigation focused on the tree layout
 	showTree: true,
-
     treeLayout: ""
-
-    
 }
-
 
 // A "ghost" layer which offsets other layers in the tree
 addNode("blank", {
     layerShown: "ghost",
-}, 
-)
+})
 
-
+// Register the main tree tab within the game engine
 addLayer("tree-tab", {
     tabFormat: [["tree", function() {return (layoutInfo.treeLayout ? layoutInfo.treeLayout : TREE_LAYERS)}]],
-    previousTab: "",
+    previousTab: "", // Left blank to allow proper back navigation
     leftTab: true,
 })
